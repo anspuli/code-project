7898fae71862c4241b94849d203e764294391212
Rigved <71826040+RgTheGreat@users.noreply.github.com>
2020-12-06 14:01:25 +0530

    Update js-sample.js

--- js/js-sample.js
+++ js/js-sample.js
@@ -1,7 +1,58 @@
-<p id="Demo"></p>
-<p id="text"></p>
-<button type="button" 
-onclick="document.getElementById("Demo").innerHTML = Date()">
-Date and Time</button>
+<!DOCTYPE html>
+<html>
+<body>
+//variable
 
-document-getElementById('text').innerHTML = "Hello";
+<p id="demo"></p>
+
+<script>
+var x = 5;
+var y = 6;
+var z = x * y;
+document.getElementById("demo").innerHTML =
+"Answer is: " + z;
+</script>
+
+//*= Operater
+
+<p id="operator"></p>
+
+<script>
+var x = 10;
+x *= 5;
+document.getElementById("operater").innerHTML = x;
+</script>
+
+
+//functions
+
+<p id="Func"></p>
+
+<script>
+function myFunction(f1, f2) {
+  return f1 * f2;
+}
+document.getElementById("Func").innerHTML = myFunction(5, 6);
+</script>
+
+//strings
+
+
+<p id="string"></p>
+
+<script>
+
+var langName1 = "JavaScript"; 
+var langName2 = 'HTML'; 
+
+document.getElementById("string").innerHTML =
+langName1 + " " + langName2; 
+
+</script>
+
+
+
+</body>
+</html>
+
+
