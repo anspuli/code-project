0a546d92398d31b6dea85c08016d0b0cd8022c39
Rigved <71826040+RgTheGreat@users.noreply.github.com>
2020-12-06 20:20:58 +0530

    Added the for loop

---
+++ js/JS-document_1.js
@@ -0,0 +1,12 @@
+
+<p id="lang"></p>
+
+<script>
+var lang = ["HTML", "JavaScript", "Python", "Bootstrap", "SQL", "PHP"];
+var text = "";
+var i;
+for (i = 0; i < lang.length; i++) {
+  text += lang[i] + "<br>";
+}
+document.getElementById("lang").innerHTML = text;
+</script>
