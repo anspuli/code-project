df5ab78f761957754cb25abb805d1493082ad11a
Rigved <71826040+RgTheGreat@users.noreply.github.com>
2020-12-07 22:18:10 +0530

    Update JS-document_1.js

--- js/JS-document_1.js
+++ js/JS-document_1.js
@@ -40,3 +40,33 @@ function myFunction() {
 }
 document.getElementById("id2").innerHTML = typeof langName;
 </script>
+
+
+
+//Date_getMethods
+
+
+<p id="demo"></p>
+
+<script>
+var d = new Date();
+document.getElementById("demo").innerHTML = d.getTime();
+</script>
+
+
+//type conversion
+<p id="TypeCon"></p>
+
+<script>
+document.getElementById("TypeCon").innerHTML = 
+  typeof "Rigved" + "<br>" +
+  typeof 3.14 + "<br>" +
+  typeof NaN + "<br>" +
+  typeof false + "<br>" +
+  typeof [1,2,3,4] + "<br>" +
+  typeof {name:'Rigved', age:9} + "<br>" +
+  typeof new Date() + "<br>" +
+  typeof function () {} + "<br>" +
+  typeof myCar + "<br>" +
+  typeof null;
+</script>
