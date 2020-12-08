3abd899b2c9986461996e298fc35cc87e9fd131c
Rigved <71826040+RgTheGreat@users.noreply.github.com>
2020-12-08 09:11:21 +0530

    Create JS-transition.js

---
+++ js/JS-transition.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style> 
+div {
+  width: 100px;
+  height: 100px;
+  background: red;
+  transition-property: width;
+  transition-duration: 5s;
+  transition-timing-function: linear;
+  transition-delay: 1s;
+}
+
+div:hover {
+  width: 300px;
+}
+</style>
+</head>
+<body>
+<div></div>
+<!--how many div tags you put that many transition you'll get!-->
+</body>
+</html>
